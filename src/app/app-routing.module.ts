f9c87c0f945182fdb5e5fdb919dc79cae1199904
Anton Chenokulskyi <chernokulskyi@gmail.com>
2022-09-07 18:22:43 +0300

    basic routing and tabs ui

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TodosComponent } from './todos/todos.component';
+import { NotesComponent } from './notes/notes.component';
+
+const routes: Routes = [
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: 'todos', component: TodosComponent },
+  { path: 'notes', component: NotesComponent },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {
+}
+
